09de1da68ee2bab9e290892f90b5344cdb226714
hueseyinsoezer <huseyinsozer97@hotmail.com>
2021-11-04 19:05:45 +0300

    +Made the plane go forward +Slowed the plane down to a manageable speed +Made the plane tilt only if the user presses the up/down arrows +Repositioning the camera so it's beside the plane now

--- Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -6,7 +6,7 @@ public class PlayerControllerX : MonoBehaviour
 {
     public float speed;
     public float rotationSpeed;
-    public float verticalInput;
+    public float horizontalInput, verticalInput;
 
     // Start is called before the first frame update
     void Start()
@@ -17,13 +17,16 @@ public class PlayerControllerX : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
         // get the user's vertical input
         verticalInput = Input.GetAxis("Vertical");
 
         // move the plane forward at a constant rate
-        transform.Translate(Vector3.back * speed);
+        transform.Translate(Vector3.up * speed*verticalInput*Time.deltaTime);
 
         // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.right * horizontalInput * rotationSpeed * Time.deltaTime);
+
+
     }
 }
